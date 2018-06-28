947d1cfaac2ec294fbbfdc82695dbe85160f1749
Zachary Pitcher <zackpi@mit.edu>
2018-06-28 05:30:57 -0400

    Added memory for left and right eyes, and pick best one of each to display

--- cam_opencv.py
+++ cam_opencv.py
@@ -1,3 +1,4 @@
+import sys
 import cv2
 import numpy as np
 from collections import deque
@@ -27,7 +28,6 @@ lefteye_mem = deque([None, ]*EYE_MEM_LEN)
 righteye_mem = deque([None, ]*EYE_MEM_LEN)
 
 while cv2.waitKey(1000//30) == -1:
-    print(face_mem)
     
     ret, frame = cam.read()
     draw = frame.copy()
@@ -35,8 +35,7 @@ while cv2.waitKey(1000//30) == -1:
     if ret:
         gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = haar_face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5)
-        
-        
+    
         if len(faces) == 0:
             face_mem.append(None)
             face_box = avg_of_valid(face_mem)
@@ -46,26 +45,23 @@ while cv2.waitKey(1000//30) == -1:
                 face_mem.popleft()
                 cv2.imshow("Full", frame)
                 continue
-                
-            
         elif len(faces) > 1:
             # use the face closest to avg of prev faces
             face_box = avg_of_valid(face_mem)
+            min_diff, min_i = sys.maxsize, 0
             if face_box:
                 px,py,pw,ph = face_box
-                min_diff, min_i = 0, 0
                 for i, (tx,ty,tw,th) in enumerate(faces):
-                    diff = (px-tx)**2+(py-ty)**2+(pw-tw)**2+(ph-th)**2
+                    diff = (px-tx)**2+(py-ty)**2+(px+pw-tx-tw)**2+(py+ph-ty-th)**2
                     if diff < min_diff:
                         min_diff = diff
                         min_i = i
-            x,y,w,h = faces[i] 
-            face_mem.append((x,y,w,h))   
+            x,y,w,h = faces[min_i] 
+            face_mem.append((x,y,w,h))
         else:    
             x,y,w,h = faces[0]
             face_mem.append((x,y,w,h))
         face_mem.popleft()
-
         face = frame[y:y+h, x:x+w]        
         cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 4)
         cv2.line(frame, (x,y+h//2), (x+w,y+h//2), (255,0,0), 2)
@@ -74,29 +70,69 @@ while cv2.waitKey(1000//30) == -1:
         gray_face = gray_img[y:y+h, x:x+w]
         eyes = haar_eye_cascade.detectMultiScale(gray_face, scaleFactor=1.1,
                                                 minNeighbors=5)
-        """        
-        for (ex,ey,ew,eh) in eyes:
-            if ey+eh//2 < h//2:
-                cv2.rectangle(frame, (x+ex,y+ey), (x+ex+ew, y+ey+eh), (0,255,0), 4)
-                
-                if ex+ew//2 < w//2: 
-                    righteye_mem.popleft()
-                    righteye_mem.append((ex, ey, ew, eh))
-                    
-                    rx,ry,rw,rh = avg_of_valid(righteye_mem)
-                    
-                    eye = face[ry:ry+rh, rx:rx+rw]
-                    cv2.imshow("RightEye", eye)
-                    
-                else:
-                    lefteye_mem.popleft()
-                    lefteye_mem.append((ex, ey, ew, eh))
-                    
-                    lx,ly,lw,lh = avg_of_valid(lefteye_mem)
-                    
-                    eye = face[ly:ly+lh, lx:lx+lw]
-                    cv2.imshow("LeftEye", eye)
-        """
+        
+        right_eyes = [(ex,ey,ew,eh) for (ex,ey,ew,eh) in eyes if ey+eh//2 < h//2 
+                                                            and ex+ew//2 < w//2]
+        if len(right_eyes) == 0:
+            righteye_mem.append(None)
+            reye_box = avg_of_valid(righteye_mem)
+            if reye_box:
+                ex,ey,ew,eh = reye_box
+            else:
+                righteye_mem.popleft()
+                continue
+        elif len(right_eyes) > 1:
+            # use the face closest to avg of prev faces
+            reye_box = avg_of_valid(righteye_mem)
+            
+            min_diff, min_i = sys.maxsize, 0
+            if reye_box:
+                px,py,pw,ph = reye_box
+                for i, (tx,ty,tw,th) in enumerate(right_eyes):
+                    diff = (px-tx)**2+(py-ty)**2+(px+pw-tx-tw)**2+(py+ph-ty-th)**2
+                    if diff < min_diff:
+                        min_diff = diff
+                        min_i = i
+            ex,ey,ew,eh = right_eyes[min_i] 
+            righteye_mem.append((ex,ey,ew,eh))
+        else:    
+            ex,ey,ew,eh = right_eyes[0]
+            righteye_mem.append((ex,ey,ew,eh))
+        cv2.rectangle(frame, (x+ex,y+ey), (x+ex+ew,y+ey+eh), (0,0,255), 2)
+        cv2.line(frame, (x+ex,y+ey+eh//2), (x+ex+ew,y+ey+eh//2), (0,0,255), 1)
+        cv2.line(frame, (x+ex+ew//2,y+ey), (x+ex+ew//2,y+ey+eh), (0,0,255), 1)
+        
+        left_eyes = [(ex,ey,ew,eh) for (ex,ey,ew,eh) in eyes if ey+eh//2 < h//2 
+                                                            and ex+ew//2 >= w//2]
+        if len(left_eyes) == 0:
+            lefteye_mem.append(None)
+            leye_box = avg_of_valid(lefteye_mem)
+            if leye_box:
+                ex,ey,ew,eh = leye_box
+            else:
+                lefteye_mem.popleft()
+                continue
+        elif len(left_eyes) > 1:
+            # use the face closest to avg of prev faces
+            leye_box = avg_of_valid(lefteye_mem)
+            
+            min_diff, min_i = sys.maxsize, 0
+            if leye_box:
+                px,py,pw,ph = leye_box
+                for i, (tx,ty,tw,th) in enumerate(left_eyes):
+                    diff = (px-tx)**2+(py-ty)**2+(px+pw-tx-tw)**2+(py+ph-ty-th)**2
+                    if diff < min_diff:
+                        min_diff = diff
+                        min_i = i
+            ex,ey,ew,eh = left_eyes[min_i] 
+            lefteye_mem.append((ex,ey,ew,eh))
+        else:    
+            ex,ey,ew,eh = left_eyes[0]
+            lefteye_mem.append((ex,ey,ew,eh))   
+        cv2.rectangle(frame, (x+ex,y+ey), (x+ex+ew,y+ey+eh), (0,0,255), 2)
+        cv2.line(frame, (x+ex,y+ey+eh//2), (x+ex+ew,y+ey+eh//2), (0,0,255), 1)
+        cv2.line(frame, (x+ex+ew//2,y+ey), (x+ex+ew//2,y+ey+eh), (0,0,255), 1)
+        
         cv2.imshow("Full", frame)
    
     else:
